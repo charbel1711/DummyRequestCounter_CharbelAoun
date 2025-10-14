eeb90fdf708b80422536b6b9a32c9981814c0fc2
charbel1711 <charbel.aoun4@net.usj.edu.lb>
2025-10-14 22:54:59 +0300

    fixing the lint errors

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -5,6 +5,7 @@ from fastapi.testclient import TestClient
 
 pytestmark = pytest.mark.integration
 
+
 def test_root_with_redis(monkeypatch):
 
     monkeypatch.setenv("REDIS_HOST", "127.0.0.1")
@@ -12,7 +13,7 @@ def test_root_with_redis(monkeypatch):
 
     import app.app as app_module
     importlib.reload(app_module)
-    
+
     with TestClient(app_module.app) as client:
         r = client.get("/")
         assert r.status_code == 200
